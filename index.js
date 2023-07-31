c4c6480a9642d2ec833afa8a0b0c908b9804eadc
Muhammad Ammad Sheikh <139677523+AmmadLala@users.noreply.github.com>
2023-07-31 14:53:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const add = 5 + 3;
+const sub = 10 - 2;
+const mult = 4 * 2;
+const divd = 16 / 2;
+console.log(`Addition:${add}`);
+console.log(`substraction:${sub}`);
+console.log(`Multiply:${mult}`);
+console.log(`Division:${divd}`);
